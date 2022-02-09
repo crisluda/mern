1c44e788a1b8c4c014db7498553b61af01ab9af8
crisluda <crisluda2244@gmail.com>
2022-02-09 13:06:42 +0000

    import colors

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import goals from "./routes/goalsRoutes.mjs";
 import { errorHandler } from "./middleware/errorMiddleware.mjs";
+import connectDB from "./database/db.mjs";
 const port = process.env.PORT || 5000;
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(express.urlencoded({ extended: false }));
 
